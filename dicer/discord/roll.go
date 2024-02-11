5d85dfdbae5cacb0dd6da9bc8b7cc6e4ba7b5b0d
Innokentiy Sokolov <keshon@zoho.com>
2024-02-12 00:00:53 +0300

    fix: calc errors; feat: layout

--- dicer/discord/roll.go
+++ dicer/discord/roll.go
@@ -3,32 +3,43 @@ package discord
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"strconv"
 	"strings"
 
 	embed "github.com/Clinet/discordgo-embed"
 	"github.com/bwmarrin/discordgo"
 	"github.com/gookit/slog"
+
+	"github.com/keshon/dice-roller/dicer/utils"
 )
 
 // handleRollCommand handles the roll command for Discord.
 func (d *Discord) handleRollCommand(s *discordgo.Session, m *discordgo.MessageCreate, param string) {
 	d.changeAvatar(s)
 
+	slog.Warn("user input is", param)
+
 	// Tokenization logic
-	tokens, signs := tokenize(param)
-	slog.Warn("Tokens are", tokens)
-	slog.Warn("Signs are", signs)
+	tokens := tokenize(param)
+
+	slog.Warn("all tokens are", tokens)
 
 	// Initialize variables for the rolling logic
 	totalResult := 0
 	detailedResults := make(map[string][]int)
 	processedKeys := []string{} // Keep track of processed keys
+	isOneMultiplier := true
 
 	// Rolling logic
 	for i, token := range tokens {
 		// Parse each token
 		multiplier, diceSides, err := parseToken(token)
+
+		slog.Infof("[%v] work with token is %v:", i, token)
+		slog.Infof("[%v] ..multiplier for token %v is %v: ", i, token, multiplier)
+		slog.Infof("[%v] ..diceSides for token %v is %v: ", i, token, diceSides)
+
 		if err != nil {
 			s.ChannelMessageSend(m.ChannelID, "Invalid input. Please use a valid dice expression, e.g., `1d20`.")
 			return
@@ -40,113 +51,73 @@ func (d *Discord) handleRollCommand(s *discordgo.Session, m *discordgo.MessageCr
 			return
 		}
 
-		// Determine sign
-		sign := "+"
-		if i < len(signs) {
-			sign = signs[i]
-		}
-
 		// Roll the dice
-		results := make([]int, multiplier)
+		results := make([]int, utils.AbsInt(multiplier))
 		for j := 0; j < multiplier; j++ {
 			rollResult := rand.Intn(diceSides) + 1
-			if sign == "-" {
-				rollResult = -rollResult
-			}
+			slog.Infof("[%v] ..rolled value is %v\n", i, rollResult)
 			results[j] = rollResult
 			totalResult += rollResult
 		}
 
 		// Store detailed results
-		var key string
-		if sign == "+" {
-			key = fmt.Sprintf("%dd%d", multiplier, diceSides)
-		} else if sign == "-" {
-			key = fmt.Sprintf("%dd%d", multiplier, diceSides)
-			results = negateResults(results)
-		}
+		key := fmt.Sprintf("%dd%d", multiplier, diceSides)
 		detailedResults[key] = results
 		processedKeys = append(processedKeys, key)
-	}
 
-	// Formatting detailed results in the correct order
-	detailedResultStr := ""
-	for _, key := range processedKeys {
-		results := detailedResults[key]
-		detailedResultStr += fmt.Sprintf("%s: (%s)\n", key, strings.Trim(strings.Join(strings.Fields(fmt.Sprint(results)), " + "), "[]"))
+		if multiplier > 1 {
+			isOneMultiplier = false
+		}
 	}
-
-	// Formatting final result
-	resultStr := fmt.Sprintf("rolling %s\n%s= %d", param, detailedResultStr, totalResult)
 
 	// Sending result to the Discord channel
 	embedMsg := embed.NewEmbed().
-		SetDescription(resultStr).
-		SetColor(0x9f00d4).MessageEmbed
+		SetTitle(fmt.Sprintf("= %d", totalResult)).
+		// SetDescription(fmt.Sprintf("Roll for`%s`:\n", param)).
+		// SetFooter(fmt.Sprintf("Roll for %s:\n", param)).
+		SetColor(0x9f00d4)
 
-	s.ChannelMessageSendEmbed(m.ChannelID, embedMsg)
-}
+	// Formatting detailed results in the correct order
+	for _, key := range processedKeys {
+		results := detailedResults[key]
 
-// negateResults negates each element in the results slice
-func negateResults(results []int) []int {
-	for i := range results {
-		results[i] = -results[i]
-	}
-	return results
-}
-
-// tokenize breaks down the input into tokens along with signs.
-func tokenize(input string) ([]string, []string) {
-	// You can implement your own logic for tokenization based on the input format.
-	// For simplicity, let's use a basic approach for now.
-	input = strings.ReplaceAll(input, " ", "")
-
-	var tokens []string
-	var signs []string
-
-	for _, char := range input {
-		if char == '+' || char == '-' {
-			signs = append(signs, string(char))
+		// Check if only one dice was requested and multiplier is also 1
+		if len(tokens) == 1 && isOneMultiplier {
+			// If only one dice with multiplier 1, don't include the value in the field
+			embedMsg.AddField("", "`"+key+"`").MakeFieldInline()
+		} else {
+			embedMsg.AddField(fmt.Sprintf("(%s)\n", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(results)), " + "), "[]")), "`"+key+"`").MakeFieldInline()
 		}
 	}
 
-	tokens = strings.FieldsFunc(input, func(r rune) bool {
-		return r == '+' || r == '-'
-	})
-
-	return tokens, signs
+	s.ChannelMessageSendEmbed(m.ChannelID, embedMsg.MessageEmbed)
 }
 
-// parseToken extracts multiplier, dice sides, and constant from a token.
+// tokenize breaks down the input into tokens along with signs.
+func tokenize(input string) []string {
+	// Define regular expression to split input based on delimiters
+	delimiters := `[^0-9d]+`
+	r := regexp.MustCompile(delimiters)
+
+	// Use regular expression to split input based on delimiters
+	tokens := r.Split(input, -1)
+
+	return tokens
+}
+
+// parseToken extracts multiplier and dice sides from a token.
 func parseToken(token string) (int, int, error) {
-	// Your implementation for parsing each token.
-	// For simplicity, let's assume the token format is "NdM+C" where N, M, and C are integers.
 	parts := strings.Split(token, "d")
 	if len(parts) != 2 {
 		return 0, 0, fmt.Errorf("Invalid token format")
 	}
 
-	multiplier, _ := strconv.Atoi(parts[0])
-
-	// Check for constant term
-	var diceSidesStr string
-	if strings.Contains(parts[1], "+") {
-		splitParts := strings.Split(parts[1], "+")
-		diceSidesStr = splitParts[0]
-	} else if strings.Contains(parts[1], "-") {
-		splitParts := strings.Split(parts[1], "-")
-		diceSidesStr = splitParts[0]
-	} else {
-		diceSidesStr = parts[1]
+	multiplier := 1
+	if parts[0] != "" {
+		multiplier, _ = strconv.Atoi(parts[0])
 	}
 
-	diceSides, _ := strconv.Atoi(diceSidesStr)
-
-	slog.Info("Token", token)
-	slog.Info("All Values detected", parts)
-	slog.Info("Value detected", parts[1])
-	slog.Info("Multiplier detected", multiplier)
-	slog.Info("diceSides detected", diceSides)
+	diceSides, _ := strconv.Atoi(parts[1])
 
 	return multiplier, diceSides, nil
 }
--- dicer/utils/utils.go
+++ dicer/utils/utils.go
@@ -109,3 +109,21 @@ func TrimString(input string, limit int) string {
 
 	return input[:limit]
 }
+
+func AbsInt(x int) int {
+	return absDiffInt(x, 0)
+}
+
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func absDiffUint(x, y uint) uint {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
